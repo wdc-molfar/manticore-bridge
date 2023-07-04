b3add532e600327b65882ede6df2687cfbfe88ed
mr-sobol <mr_sobol@ukr.net>
2023-07-04 15:32:06 +0300

    Add New Features

--- src/bridge.js
+++ src/bridge.js
@@ -7,18 +7,17 @@ const { isString, keys } = require("lodash")
 
 let client = {}
 
-
 const getClient = url => {
     if(client[url]) return client[url]
     
     apiClient = new Manticoresearch.ApiClient()
     apiClient.basePath = url
     apiClient.timeout = process.env.TIMEOUT ? process.env.TIMEOUT : 30000
-    
-    client[url] = new Manticoresearch.UtilsApi(apiClient)
+    client[url] = apiClient
 
     return client[url]
 }
+
 
 /** Функція, що дозволяє обійти json структуру і замінити дані, щоб зберігати json в БД
  * @param {String}, {items} дані, що прийшли на аналіз
@@ -38,6 +37,38 @@ const formatter = (items) => {
     }
     return items
 }
+/** Повертає результат виконання запиту в manticoreSearch (update)
+ * @param {String},{Object}    param для підключення manticoreSearch, може бути посилання на клієнт або url
+ * @param {Object} query       SQL запит
+ * @return {Promise}
+ */
+const update = async (query, param) =>{
+    try{
+        const client = (isString(param)) ? getClient(param) : param
+        const indexApi = new Manticoresearch.IndexApi(client)
+        res = await indexApi.update(query);
+        const { ...content } = res
+        return content
+    } catch (e) { 
+        throw e
+    } 
+}
+/** Повертає результат виконання запиту в manticoreSearch (insert)
+ * @param {String},{Object}    param для підключення manticoreSearch, може бути посилання на клієнт або url
+ * @param {Object} query       SQL запит
+ * @return {Promise}
+ */
+const insert = async (query, param) =>{
+    try{
+        const client = (isString(param)) ? getClient(param) : param
+        const indexApi = new Manticoresearch.IndexApi(client)
+        res = await indexApi.insert(query);
+        const { ...content } = res
+        return content
+    } catch (e) { 
+        throw e
+    } 
+}
 
 /** Повертає результат виконання запиту в manticoreSearch 
  * @param {String},{Object}    param для підключення manticoreSearch, може бути посилання на клієнт або url
@@ -48,7 +79,8 @@ const formatter = (items) => {
 const execute = async (query, param) => {
     try{
         const client = (isString(param)) ? getClient(param) : param
-        const res = await client.sql(query)
+        const utilsApi = new Manticoresearch.UtilsApi(client)
+        const res = await utilsApi.sql(query)
         const { ...content } = res
         return content
     } catch (e) { 
@@ -69,5 +101,7 @@ module.exports = {
     getClient,
     execute,
     stringify,
-    formatter
+    formatter,
+    insert,
+    update
   };
